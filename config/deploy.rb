a00b17319e7e4947aa5b5d53ed7021d60d4a2700
Chidambara Bharathi <cbharathi@anpi.com>
2015-07-13 18:13:58 +0000

    Deployment settings change

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :default_stage, 'staging'
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'deployment_demo'
+set :application, 'deployment'
 set :repo_url, 'git@github.com:bharathimit/deployment_demo.git'
 
 # Default branch is :master
